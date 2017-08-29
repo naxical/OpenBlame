58cce0d205cf23b1b23e2b084405e0f60a4a42ea
Nax <31314269+naxical@users.noreply.github.com>
2017-08-28 20:25:32 -0400

    Add files via upload

---
+++ passgen/README.txt
@@ -0,0 +1,9 @@
+First of all, thanks for downloading passgen.
+
+There is a few things you need to know before starting the program.
+First, make sure you have python 2.7 installed. PYTHON 3 MAY NOT WORK SO MAKE SURE TO DOWNLOAD PYTHON 2.7!!!
+After making sure you have python installed you are then able to run the program through a terminal window.
+
+Before you run it I recommend making a few changes to make the process easier. Inside the passgen folder there should be 4 files, one file is a script.
+If you open it nothing will happen until you run it in a terminal window. Do not edit or delete the file. First copy the script file to your user directory, this ensures that when you run the script you will not have to change directories.
+After you have done this you can now run the script. To run it open a terminal window and type ./passgenscript the script should run and you will then be brought to the program.
---
+++ passgen/passgen.py
@@ -0,0 +1,40 @@
+# Program Made To Generate Secure Password Phrases
+
+import random
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#&*^!%$~"
+pw_length = 10
+mypw = ""
+
+for i in range(pw_length):
+    next_index = random.randrange(len(alphabet))
+    mypw = mypw + alphabet[next_index]
+
+passtype = raw_input("What service are you using? ")
+print passtype
+line = "Your password for " + passtype + " is : " + mypw
+print line
+
+writeyn = raw_input("Would you like to write your password to a text file? Y/n")
+writeyn = writeyn.strip() # makes sure that no white space is around
+
+if writeyn == 'Y':
+    print('Written to passwords.txt')
+    f = open("passwords.txt", "a+") # "append"
+    f.write(passtype + " - " + mypw)
+
+if writeyn == 'n':
+    print('Writting Stopped.')
+
+while writeyn != 'n' and writeyn != 'Y':
+    writeyn = raw_input("Invalid Input, Please Enter Y or n; " + "\nWould you like to write your password to a text file? Y/n")
+    writeyn = writeyn.strip()
+    if writeyn == 'n' or writeyn == 'N' or writeyn == 'no' or writeyn == 'No':
+        print 'Writing Stopped.'
+        break
+    elif writeyn == 'Y' or writeyn == 'y' or writeyn == 'yes' or writeyn == 'Yes':
+        print('Written to passwords.txt')
+        f = open("passwords.txt", "a+")
+        f.write(passtype + " - " + mypw)
+        break
